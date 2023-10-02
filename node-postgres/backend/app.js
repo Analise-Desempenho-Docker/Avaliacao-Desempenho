a3e0ddabe90301b965c896e82ba54ed87b184a71
MarcosGTS <marcosgtsodre@gmail.com>
2023-10-02 09:02:56 -0300

    Create basic database connection with Sequelizer

--- node-postgres/backend/app.js
+++ node-postgres/backend/app.js
@@ -1,25 +1,31 @@
+const { sequelize } = require("./models");
 const server = require("express");
 const bodyParser = require("body-parser");
-const db = require("./queries.js");
+// const db = require("./queries.js");
 const app = server();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
-app.get('/', (request, response) => {
-    response.json({
-        info: "Basic movies API", 
-        paths: ["/movies", "/movies/:id"]
-    })
-})
+app.get("/", (request, response) => {
+  response.json({
+    info: "Basic movies API",
+    paths: ["/movies", "/movies/:id"],
+  });
+});
 
-app.get('/movies', db.getAll);
-app.get('/movies/:id', db.getById);
-app.post('/movies', db.create);
+// app.get("/movies", db.getAll);
+// app.get("/movies/:id", db.getById);
+// app.post("/movies", db.create);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, async () => {
+  console.log(`Listening on port ${PORT}`);
+
+  await sequelize.sync({ force: true });
+  console.log("Sequelize synced");
+});
--- node-postgres/database/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Student(
-       id SERIAL PRIMARY KEY, 
-       name VARCHAR(60), 
-       age INT
-);
-INSERT INTO Student(name, age) VALUES ('Shubham verma', 21);
-INSERT INTO Student(name, age) VALUES ('Utkarsh verma', 23);
