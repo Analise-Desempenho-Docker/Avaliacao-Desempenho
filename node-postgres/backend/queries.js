2a0d3906a1cbb13771259f427334ad504a308982
MarcosGTS <marcosgtsodre@gmail.com>
2023-10-02 11:34:58 -0300

    Refactor `queries.js`

--- node-postgres/backend/queries.js
+++ node-postgres/backend/queries.js
@@ -1,38 +1,44 @@
-const Client = require("pg").Client;
-const client = new Client({
-    user: process.env.DB_USER,
-    host: "postgres-db",
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-})
+const { Movies } = require("./models");
 
-client.connect().then(() => console.log("Connected"))
+// const Client = require("pg").Client;
+// const client = new Client({
+//   user: process.env.DB_USER,
+//   host: "postgres-db",
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 const getAll = async (request, response) => {
-    const result = await client.query("SELECT * from Student ORDER BY id")
-    
-    response.json(result.rows)
-}
+  const result = await Movies.findAll();
+
+  response.json(result);
+};
 
 const getById = async (request, response) => {
-    const id = parseInt(request.params.id)
-    const result = await client.query("SELECT * from Student WHERE id = $1", [id]) 
-    
-    response.json(result.rows)
-}
+  const { id } = request.params;
+
+  try {
+    const result = await Movies.findOne({ where: { id } });
+    response.json(result);
+  } catch (err) {
+    response.status(500).json({ err });
+  }
+};
 
 const create = async (request, response) => {
-    const {name, age} = request.body.data; 
-    console.log(name, age);
+  const { name, storyline, rating } = request.body;
 
-    const result = await client.query("INSERT INTO Student(name, age) VALUES ($1, $2)", [name, age]);
-    
+  try {
+    const result = await Movies.create({ name, storyline, rating });
     response.json(result);
-}
+  } catch (err) {
+    response.status(500).json({ err });
+  }
+};
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-}
+  getAll,
+  getById,
+  create,
+};
